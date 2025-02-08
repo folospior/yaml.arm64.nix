3f67f7f69190c66a47638116f14f3d5016c39b29
Filip Hoffmann <folosp@protonmail.com>
2025-02-08 02:28:39 +0100

    change amd64 to aarch
    im just as lazy as the other guy

--- modules/convert.nix
+++ modules/convert.nix
@@ -7,5 +7,5 @@ in derivation {
   name = "yaml-convert";
   builder = env;
   args = [ "file=${inputFile}" bash "-c" "${command} >$out" ];
-  system = "x86_64-linux";
+  system = "aarch64-linux";
 }
